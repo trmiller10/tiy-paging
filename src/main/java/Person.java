b97e28a9c45bd2e030bd1323f7a540ed9d05ecbe
Taylor Miller <trmiller10@gmail.com>
2016-05-15 13:12:32 -0400

    Created a program that reads a .csv file, parses it, and displays the contents in HTML that can be paged through

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -20,4 +20,52 @@ public class Person {
 
     // create a public property for ipAddress
     String ipAddress;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
 }
